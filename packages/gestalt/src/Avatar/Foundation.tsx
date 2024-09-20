bdd8e62f816c6c340ef464b4302f70d1d3cb5ace
Sarah Alli <salli@pinterest.com>
2024-09-20 14:35:32 -0400

    working

--- packages/gestalt/src/Avatar/Foundation.tsx
+++ packages/gestalt/src/Avatar/Foundation.tsx
@@ -7,15 +7,19 @@ import avatarStyles from '../AvatarGroup.css';
 import styles from '../Icon.css';
 import icons from '../icons/index';
 import vrIcons from '../icons-vr-theme/index';
+import TextUI from '../TextUI';
 import typographyStyle from '../Typography.css';
 import useInExperiment from '../useInExperiment';
-import TextUI from '../'
 
 const ICON_SIZE_RATIO = (20 / 48) * 100; // For pixel perfect icon button, we use the icon (20px) to parent container (48px) size ratio
 
 type ResponsiveFitSizeBoxProps = {
   avatarColorIndex?: string;
   children: ReactNode;
+  isFocused?: boolean;
+  isFocusVisible?: boolean;
+  isHovered?: boolean;
+  isPressed?: boolean;
   outline: boolean;
 };
 
@@ -45,9 +49,13 @@ function ResponsiveFitSizeBox({ avatarColorIndex, children, outline }: Responsiv
   );
 }
 
-type Props = {
+type AvatarFoundationProps = {
   avatarColorIndex?: string;
   children?: string | number;
+  isFocused?: boolean;
+  isFocusVisible?: boolean;
+  isHovered?: boolean;
+  isPressed?: boolean;
   fontSize?: string;
   outline?: boolean;
   textAnchor?: 'start' | 'middle' | 'end';
@@ -65,11 +73,11 @@ export default function AvatarFoundation({
   title,
   translate,
   content = 'text',
-}: Props) {
+}: AvatarFoundationProps) {
 
   const cs = classnames(styles.icon, avatarStyles.text);
 
-  const isInExperiment = useInExperiment({
+  const isInVRExperiment = useInExperiment({
     webExperimentName: 'web_gestalt_visualRefresh',
     mwebExperimentName: 'web_gestalt_visualRefresh',
   });
@@ -85,20 +93,26 @@ export default function AvatarFoundation({
           xmlns="http://www.w3.org/2000/svg"
         >
           {title ? <title>{title}</title> : null}
-          <text
-            className={[
-              typographyStyle.antialiased,
-              typographyStyle.sansSerif,
-              typographyStyle.fontWeightSemiBold,
-              translate && avatarStyles[translate], // if addCollaborator button is present, translateX moves numbers closer to the edge
-            ].join(' ')}
-            dy="0.35em"
-            fill={TOKEN_COLOR_TEXT_DEFAULT}
-            fontSize={fontSize}
-            textAnchor={textAnchor}
-          >
-            {children}
-          </text>
+          {isInVRExperiment ? (
+            <TextUI align="center" size="lg">
+              {children}
+            </TextUI>
+          ) : (
+            <text
+              className={[
+                typographyStyle.antialiased,
+                typographyStyle.sansSerif,
+                typographyStyle.fontWeightSemiBold,
+                translate && avatarStyles[translate], // if addCollaborator button is present, translateX moves numbers closer to the edge
+              ].join(' ')}
+              dy="0.35em"
+              fill={TOKEN_COLOR_TEXT_DEFAULT}
+              fontSize={fontSize}
+              textAnchor={textAnchor}
+            >
+              {children}
+            </text>
+          )}
         </svg>
       ) : null}
       {content === 'icon' ? (
@@ -112,7 +126,7 @@ export default function AvatarFoundation({
           xmlns="http://www.w3.org/2000/svg"
         >
           <title>Icon</title>
-          <path d={(isInExperiment ? vrIcons : icons)['person-add']} />
+          <path d={(isInVRExperiment ? vrIcons : icons)['person-add']} />
         </svg>
       ) : null}
     </ResponsiveFitSizeBox>
